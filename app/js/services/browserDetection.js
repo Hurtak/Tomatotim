dd89a0a2ba6766c36a9e0627b3bcc821b5c18ef9
Hurtak <petr.hurtak@gmail.com>
2015-01-05 20:06:12 +0100

    browserDetection: added

---
+++ app/js/services/browserDetection.js
@@ -0,0 +1,20 @@
+var browserDetection = (function() {
+  'use strict';
+
+  // browser detection from http://stackoverflow.com/a/9851769/2955574
+
+  var isOpera = !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+  var isFirefox = typeof InstallTrigger !== 'undefined';
+  var isSafari = Object.prototype.toString.call(window.HTMLElement).indexOf('Constructor') > 0;
+  var isChrome = !!window.chrome && !isOpera;
+  var isIE = /*@cc_on!@*/false || !!document.documentMode;
+
+  return {
+    isOpera: isOpera,
+    isFirefox: isFirefox,
+    isSafari: isSafari,
+    isChrome: isChrome,
+    isIE: isIE
+  };
+
+}());
