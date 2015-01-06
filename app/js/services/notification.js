afd03585e6e939f8ff56446fa45a09f00c7a9190
Hurtak <petr.hurtak@gmail.com>
2015-01-06 19:19:39 +0100

    notifications: refactoring

--- app/js/services/notification.js
+++ app/js/services/notification.js
@@ -2,25 +2,26 @@ services.notification = (function() {
   'use strict';
 
   var notificationTimeout = 5; // seconds
-  var notificationsSupported = false;
 
   var init = function() {
+    if (!window.Notification) {
+      return;
+    }
 
-    if (typeof Notification === 'function' && Notification.permission !== 'granted') {
+    if (Notification.permission !== 'granted') {
       Notification.requestPermission();
-      notificationsSupported = true;
     }
 
   };
 
-  var newNotification = function (message, iconType) {
-    if (!notificationsSupported) {
+  var newNotification = function(message, iconType) {
+    if (!window.Notification) {
       return;
     }
 
     var notification = new Notification(config.appName, {
       icon: 'icons/favicon-' + iconType + '.ico',
-      body: message,
+      body: message
     });
 
     notification.onshow = function () {
