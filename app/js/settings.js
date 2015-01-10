bcfd583f27bda3f853b7ff650f0a03501a849c0b
Hurtak <petr.hurtak@gmail.com>
2015-01-10 13:27:53 +0100

    settings: removed debug variables

--- app/js/settings.js
+++ app/js/settings.js
@@ -2,7 +2,9 @@ var settings = (function() {
   'use strict';
 
   var init = function() {
+
     // update config defaults with saved settings (if avaliable)
+
     var audio = services.storage.get('audioNotifications');
     if (audio !== null) {
       config.audioNotifications = audio;
@@ -11,12 +13,6 @@ var settings = (function() {
     var notifications = services.storage.get('webNotifications');
     if (notifications !== null) {
       config.webNotifications = notifications;
-      if (notifications === true) {
-        services.notification.requestPermission();
-      }
-    }
-    if (config.webNotifications === true) {
-      services.notification.requestPermission();
     }
 
     config.workInterval = services.storage.get('workInterval') || config.workInterval;
@@ -26,22 +22,18 @@ var settings = (function() {
     config.repeat = services.storage.get('repeat') || config.repeat;
 
     // update settings view
+
     views.settings.audioNotifications.checked = config.audioNotifications;
     views.settings.webNotifications.checked = config.webNotifications;
 
-    if (config.debug) {
-      views.settings.workInterval.value = config.workInterval;
-      views.settings.breakInterval.value = config.breakInterval;
-      views.settings.longbreakInterval.value = config.longbreakInterval;
-    } else {
-      views.settings.workInterval.value = config.workInterval / 60;
-      views.settings.breakInterval.value = config.breakInterval / 60;
-      views.settings.longbreakInterval.value = config.longbreakInterval / 60;
-    }
+    views.settings.workInterval.value = config.workInterval / 60;
+    views.settings.breakInterval.value = config.breakInterval / 60;
+    views.settings.longbreakInterval.value = config.longbreakInterval / 60;
 
     views.settings.repeat.value = config.repeat;
 
     // binding
+
     views.settings.audioNotifications.addEventListener('blur', function() {
       config.audioNotifications = this.checked;
       services.storage.set('audioNotifications', config.audioNotifications);
@@ -55,30 +47,17 @@ var settings = (function() {
     });
 
     views.settings.workInterval.addEventListener('blur', function() {
-      if (config.debug) {
-        config.workInterval = this.value * 1;
-      } else {
-        config.workInterval = this.value * 60;
-      }
+      config.workInterval = this.value * 60;
       services.storage.set('workInterval', config.workInterval);
-
       timer.updateIntervals();
     });
     views.settings.breakInterval.addEventListener('blur', function() {
-      if (config.debug) {
-        config.breakInterval = this.value * 1;
-      } else {
-        config.breakInterval = this.value * 60;
-      }
+      config.breakInterval = this.value * 60;
       services.storage.set('breakInterval', config.breakInterval);
       timer.updateIntervals();
     });
     views.settings.longbreakInterval.addEventListener('blur', function() {
-      if (config.debug) {
-        config.longbreakInterval = this.value * 1;
-      } else {
-        config.longbreakInterval = this.value * 60;
-      }
+      config.longbreakInterval = this.value * 60;
       services.storage.set('longbreakInterval', config.longbreakInterval);
       timer.updateIntervals();
     });
@@ -88,6 +67,12 @@ var settings = (function() {
       services.storage.set('repeat', config.repeat);
       timer.updateIntervals();
     });
+
+    // request permisions
+
+    if (config.webNotifications === true) {
+      services.notification.requestPermission();
+    }
 
   };
 
